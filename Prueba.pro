c3d40ca98ab34a21a1dba649d86243f241b70a84
JulianCamacho <jjulian.341@gmail.com>
2020-02-21 00:08:42 -0600

    Ventana básica de la aplicación

--- Prueba.pro
+++ Prueba.pro
@@ -1,7 +1,5 @@
-TEMPLATE = app
-CONFIG += console c++11
-CONFIG -= app_bundle
-CONFIG -= qt
+QT += core gui
+greaterThan(QT_MAJOR_VERSION, 4) : QT += widgets
 
 SOURCES += \
         main.cpp
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
+#include <QApplication>
+#include <QLabel>
+#include <QWidget>
 
-using namespace std;
-//dkjahdka
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main(int argc, char *argv[ ]) {
+    QApplication app(argc, argv);
+    QLabel hello("Hello World");
+    hello.setWindowTitle("My First Qt Program");
+    hello.resize(400, 400);
+    hello.show();
+    return app.exec();
 }
