a6df4c0b41672c9a6c32b9c8bf7c845bb993a16c
JulianCamacho <jjulian.341@gmail.com>
2020-02-21 00:59:48 -0600

    Avances en la interfaz

--- Prueba.pro
+++ Prueba.pro
@@ -1,5 +1,6 @@
 QT += core gui
 greaterThan(QT_MAJOR_VERSION, 4) : QT += widgets
+QT += quickcontrols2
 
 SOURCES += \
         main.cpp
--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <QApplication>
 #include <QLabel>
 #include <QWidget>
+#include <QPushButton>
+#include <QTextEdit>
 
 int main(int argc, char *argv[ ]) {
     QApplication app(argc, argv);
-    QLabel hello("Hello World");
+    QLabel hello("Ingrese dos nodos para calcular el camino más corto entre ellos");
     hello.setWindowTitle("My First Qt Program");
     hello.resize(400, 400);
+    hello.setAlignment(Qt::AlignHCenter);
     hello.show();
+
+    QPushButton button("Calcular Djikstra");
+    button.show();
+
     return app.exec();
 }
